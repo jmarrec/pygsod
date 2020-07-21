012f7053866a7b4314889fbeadbf3210fbf51afe
Hang Li <hli@altanova.io>
2020-07-21 16:13:35 -0400

    try run_gsodpy.py

--- run_gsodpy.py
+++ run_gsodpy.py
@@ -1,4 +1,23 @@
 """Module to launch in the docker."""
+import json
+import os
 
 if __name__ == '__main__':
-	print("success!")
+
+	folder_input = 'input/'
+	files = os.listdir(folder_input)
+
+    list_json_files = []
+    for f in files:
+        fname, ext = os.path.splitext(f)
+        if ext == ".json":
+            list_json_files.append((fname, ext))
+
+	file_name_input = os.path.join(folder_input,
+                                   '{}{}'.format(fname, ext))
+
+    with open(file_name_input) as json_file:
+    	args = json.load(json_file)
+
+    print(args)
+

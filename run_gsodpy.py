8971c3c7f913e79a6e272d0f15ac0b98e8ffa9d6
Hang Li <hli@altanova.io>
2020-07-21 16:19:22 -0400

    try run_gsodpy.py

--- run_gsodpy.py
+++ run_gsodpy.py
@@ -7,17 +7,17 @@ if __name__ == '__main__':
 	folder_input = 'input/'
 	files = os.listdir(folder_input)
 
-    list_json_files = []
-    for f in files:
-        fname, ext = os.path.splitext(f)
-        if ext == ".json":
-            list_json_files.append((fname, ext))
+	list_json_files = []
+	
+	for f in files:
+		fname, ext = os.path.splitext(f)
+		if ext == ".json":
+			list_json_files.append((fname, ext))
 
 	file_name_input = os.path.join(folder_input,
-                                   '{}{}'.format(fname, ext))
+		'{}{}'.format(fname, ext))
 
-    with open(file_name_input) as json_file:
-    	args = json.load(json_file)
+	with open(file_name_input) as json_file:
+		args = json.load(json_file)
 
-    print(args)
-
+	print(args)

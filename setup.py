729afd49ab2b9e46a2d898575f9441701964262a
lmailhol <lmailhol@altanova.io>
2022-07-18 10:11:54 -0400

    clean standard packages in setup

--- setup.py
+++ setup.py
@@ -83,11 +83,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas', 'pathlib',
+    install_requires=['pandas',
                       'pyepw',
                       'bs4',
-                      'requests',
-                      'datetime'],
+                      'requests'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

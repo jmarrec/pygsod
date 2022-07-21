2a175e96142e4c8e4f75138456577f72209364b2
Julien Marrec <julien.marrec@gmail.com>
2022-07-21 17:10:31 +0200

    Add a  lint stage

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -20,98 +21,74 @@ LONG_DESCRIPTION = """A Wrapper to Download data off NOAA's Global Summary of th
 and load them into pandas"""
 
 setup(
-    name='pygsod',
-
+    name="pygsod",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',
-
-    description=("A Wrapper to Download data off "
-                 "NOAA's Global Summary of the day,"
-                 "and load them into pandas"),
+    version="0.2.0",
+    description=("A Wrapper to Download data off " "NOAA's Global Summary of the day," "and load them into pandas"),
     long_description=LONG_DESCRIPTION,  # TODO
-
     # The project's main homepage.
     # url='https://github.com/pypa/sampleproject',
-
     # Author details
-    author='Julien Marrec & EffiBEM',
-    author_email='contact@effibem.com',
-    url= "https://effibem.com",
+    author="Julien Marrec & EffiBEM",
+    author_email="contact@effibem.com",
+    url="https://effibem.com",
     project_urls={
         "Bug Tracker": "https://github.com/jmarrec/pygsod/issues",
         "Source Code": "https://github.com/jmarrec/pygsod",
     },
-
-
     # Choose your license
-    license='MIT',
-
+    license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Natural Language :: English',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering',
-
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
-
     # What does your project relate to?
-    keywords='gsod weather python pandas epw energyplus',
-
+    keywords="gsod weather python pandas epw energyplus",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'doc', 'tests', 'support',
-                                    'weather_files']),
-
+    packages=find_packages(exclude=["contrib", "doc", "tests", "support", "weather_files"]),
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #   py_modules=["my_module"],
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas', 'openpyxl',
-                      'pyepw',
-                      'tqdm',
-                      'requests'],
-
+    install_requires=["pandas", "openpyxl", "pyepw", "tqdm", "requests"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['black', 'mypy', 'isort', 'flake8'],
-        'test': ['coverage', 'pytest', 'pytest-cov'],
+        "dev": ["black", "isort", "flake8", "mypy", "types-setuptools", "types-requests", "pandas-stubs"],
+        "test": ["coverage", "pytest", "pytest-cov"],
     },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     # package_data={
     #     'sample': ['package_data.dat'],
     # },
-
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
